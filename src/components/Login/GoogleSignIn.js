53efd0afbafacc01e29dc0a4bb3cfca509b652dd
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2021-02-28 20:50:32 -0500

    sign up/in complete

--- src/components/Login/GoogleSignIn.js
+++ src/components/Login/GoogleSignIn.js
@@ -1,13 +1,40 @@
 import React, { cloneElement } from 'react'
 import {useHistory} from 'react-router-dom'
 import { GoogleLogin } from 'react-google-login';
+import { useDispatch } from "react-redux";
+import { setCurrentUser } from "../../features/userSlice"
 
 
 function GoogleSignIn (){
 
-    const responseGoogle = (response) => {
-        console.log(response)
-        console.log(response.profileObj)
+    const history = useHistory()
+    const dispatch = useDispatch()
+
+    const failedResponse = (res) => {
+        alert("There was an issue signing in through Google. Please try again.")
+    }
+
+    const responseGoogle = (res) => {
+        const google_id = {google_id: res.profileObj.googleId}
+
+        fetch(`${process.env.REACT_APP_RAILS_URL}googlelogin`, {
+            method: 'POST',
+            headers: {"Content-Type": 'application/json'},
+            body: JSON.stringify(google_id)
+        })
+            .then(res=>res.json())
+            .then(data => {
+                if (data.errors) {
+                    data.errors.forEach(error=>alert(error))
+                }
+                else {
+                    const { user, token } = data;
+                    const action = setCurrentUser(user);
+                    dispatch(action)
+                    localStorage.setItem("token", token);
+                    history.push("/profile");
+                }
+            })
     }
 
 
@@ -17,7 +44,7 @@ function GoogleSignIn (){
                 clientId={process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID}
                 buttonText="Sign In with Google"
                 onSuccess={responseGoogle}
-                onFailure={responseGoogle}
+                onFailure={failedResponse}
                 cookiePolicy={'single_host_origin'}
             />
         </div>
--- src/components/Sign-Up/GoogleSignUp.js
+++ src/components/Sign-Up/GoogleSignUp.js
@@ -1,12 +1,47 @@
 import { useHistory } from 'react-router-dom'
-import React from 'react'
+import React, {useState} from 'react'
 import { GoogleLogin } from 'react-google-login';
+import { useDispatch } from "react-redux";
+import { setCurrentUser } from "../../features/userSlice"
 
 function GoogleSignUp () {
 
-    const responseGoogle = (response) => {
-        console.log(response)
-        console.log(response.profileObj)
+
+    const history = useHistory()
+    const dispatch = useDispatch()
+
+    const failedResponse = (res) => {
+        alert("There was an issue signing up through Google. Please try again.")
+    }
+
+    const responseGoogle = (res) => {
+        const newUser = {
+            email: res.profileObj.email,
+            password: `${process.env.REACT_APP_GOOGLE_OAUTH_PASSWORD}`,
+            first_name: res.profileObj.givenName,
+            last_name: res.profileObj.familyName,
+            photo_url: res.profileObj.imageUrl,
+            google_id: res.profileObj.googleId
+        }
+
+        fetch(`${process.env.REACT_APP_RAILS_URL}signup`, {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(newUser)
+        })
+            .then(res=>res.json())
+            .then(data => {
+               if (data.errors) {
+                   data.errors.forEach(error=>{alert(error + ". Please try again.")})
+               } else {
+                   const {user, token} = data
+                   const action = setCurrentUser(user);
+                   dispatch(action)
+                   localStorage.setItem("token", token)
+                   history.push("/profile")
+               }
+            })
+
     }
 
     return (
@@ -15,7 +50,7 @@ function GoogleSignUp () {
                 clientId={process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID}
                 buttonText="Sign Up with Google"
                 onSuccess={responseGoogle}
-                onFailure={responseGoogle}
+                onFailure={failedResponse}
                 cookiePolicy={'single_host_origin'}
             />
         </div>
